bd6c38d4c862082443f5559c4bd95e346c3a26fa
Adam Kucera <adam.kucera@wrent.cz>
2013-09-13 20:03:13 +0200

    Files for 'easy' import.

--- includes/functions/db_import/index.php
+++ includes/functions/db_import/index.php
@@ -2,6 +2,14 @@
 //first, import the tables, with no foreign keys
 //category, menu, highlight
 
+require_once '../../classes/CDatabase.php';
+
+$database_old = new CDatabase("localhost", "wowherniweb", "woWKo4-db;15", "webcore");
+$database_old->Connect();
+
+$database_new = new CDatabase("localhost", "root", "root", "aurora");
+$database_new->Connect();
+
 include "category.php";
 include "menu.php";
 include "highlight.php";
